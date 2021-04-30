1301f09de84c2d01e7460b762c062788943d732d
Bruno Santos <47404083+BrunoSantosRJ@users.noreply.github.com>
2021-04-30 01:26:22 -0300

    Add files via upload

---
+++ DOM e selecao de elementos/script.js
@@ -0,0 +1,33 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href)
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+console.log(document.getElementsByClassName("ativo")[0]);
+
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
+
+// Retorne a largura da janela 
+console.log(window.innerWidth);
+
+
+
+// Retorne no console todas as imagens do site
+const Todasimagens = document.getElementsByTagName("img");
+console.log(Todasimagens);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imgimagem = document.querySelectorAll('[src^="img/imagem"]');
+console.log(imgimagem);
+
+// Selecione todos os links internos (onde o href começa com #)
+const Linksinternos = document.querySelectorAll('[href^="#"]');
+console.log(Linksinternos);
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const primeiroh2 = document.querySelector(".animais-descricao h2");
+console.log(primeiroh2);
+
+// Selecione o último p do site
+const ultimoP = document.querySelectorAll("p");
+console.log(ultimoP[ultimoP.length-1]);
